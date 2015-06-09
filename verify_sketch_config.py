4d17a6b6cd6de043cfd40cd3a62e5dcf869560b4
Matthew Beckler <matthew.beckler@gmail.com>
2015-06-08 20:56:00 -0500

    upgraded to the latest python script for verification

--- verify_sketch_config.py
+++ verify_sketch_config.py
@@ -23,95 +23,93 @@
 # We read in the specified ino file, make modification, and write it back out.
 # This avoid issues with relative paths and stuff like that.
 #
-# Usage: verify_sketch_config.py /path/to/sketch.ino
+# Usage: verify_sketch_config.py
+# Iterates through ./examples/* to build them all.
 
 import sys
+import os
 import subprocess
 import re
 
 # Update this variable to point to your Arduino binary
 ARDUINO = "/home/matthew/bin/arduino"
 
-RE_CONFIG = "^// Config (\d+) - (.*)$"
+def RunAllConfigs( ino ):
 
-def EnableConfigSet( original, ii ):
-    InsideDesiredConfigBlock = False
-    modified = []
-    board = ""
+    RE_CONFIG = "^// Config (\d+) - (.*)$"
 
-    for line in original:
-        if InsideDesiredConfigBlock:
-            if line.strip() == "// Config end":
-                InsideDesiredConfigBlock = False
-                modified.append(line)
+    print "*" * 80
+    print "Reading file '%s'" % ino
+
+    def EnableConfigSet( original, ii ):
+        InsideDesiredConfigBlock = False
+        modified = []
+        board = ""
+
+        for line in original:
+            if InsideDesiredConfigBlock:
+                if line.strip() == "// Config end":
+                    InsideDesiredConfigBlock = False
+                    modified.append(line)
+                else:
+                    # remove leading //
+                    modified.append(line.strip().strip("//") + "\n")
             else:
-                # remove leading //
-                modified.append(line.strip().strip("//") + "\n")
-        else:
-            # If we are not in an active config block, always use line unchanged
-            modified.append(line)
-            # check if we are starting a new config block
-            x = re.search(RE_CONFIG, line.strip())
-            if x:
-                config_set = int(x.group(1))
-                if config_set == ii:
-                    # This is the config set we want, enable it
-                    InsideDesiredConfigBlock = True
-                    board = x.group(2)
+                # If we are not in an active config block, always use line unchanged
+                modified.append(line)
+                # check if we are starting a new config block
+                x = re.search(RE_CONFIG, line.strip())
+                if x:
+                    config_set = int(x.group(1))
+                    if config_set == ii:
+                        # This is the config set we want, enable it
+                        InsideDesiredConfigBlock = True
+                        board = x.group(2)
 
-    return modified, board
+        return modified, board
 
-if len(sys.argv) < 2:
-    print "Usage: %s /path/to/sketch.ino" % sys.argv[0]
-    sys.exit(1)
+    # Read in original sketch file
+    with open(ino, "r") as fid:
+        original = fid.readlines()
 
-ino = sys.argv[1]
+    num_config_sets = 0
+    for line in original:
+        x = re.search(RE_CONFIG, line.strip())
+        if x:
+            config_set = int(x.group(1))
+            #print "%d, '%s'" % (config_set, line.strip())
+            num_config_sets = max(num_config_sets, config_set)
+    print "Found %d config sets" % num_config_sets
 
-# Read in original sketch file
-with open(ino, "r") as fid:
-    original = fid.readlines()
+    # We really, really don't want to mess up the original file on disc
+    try:
+        for ii in range(1, num_config_sets + 1):
+            x = "===== Processing config set %d " % ii
+            x += "=" * (80 - len(x))
+            print x
+            modified, board = EnableConfigSet( original, ii )
+            with open(ino, "w") as fid:
+                fid.write("".join(modified))
+            #try:
+            args = [ARDUINO, "--verify", "--board", board, ino]
+            print " ".join(args)
+            retcode = subprocess.call(args)
+            if retcode != 0:
+                print "Error while running Arduino, exiting"
+                sys.exit(1)
+            #except OSError as e:
+                #print "Exeception while running Arduino:", e
+                #sys.exit(1)
 
-num_config_sets = 0
-for line in original:
-    x = re.search(RE_CONFIG, line.strip())
-    if x:
-        config_set = int(x.group(1))
-        #print "%d, '%s'" % (config_set, line.strip())
-        num_config_sets = max(num_config_sets, config_set)
-print "Found %d config sets" % num_config_sets
-
-# We really, really don't want to mess up the original file on disc
-try:
-    for ii in range(1, num_config_sets + 1):
-        x = "===== Processing config set %d " % ii
-        x += "=" * (80 - len(x))
-        print x
-        modified, board = EnableConfigSet( original, ii )
+    finally:
+        # restore the original file
+        print "Restoring original sketch file"
         with open(ino, "w") as fid:
-            fid.write("".join(modified))
-        #try:
-        args = [ARDUINO, "--verify", "--board", board, ino]
-        print " ".join(args)
-        retcode = subprocess.call(args)
-        if retcode != 0:
-            print "Error while running Arduino, exiting"
-            sys.exit(1)
-        #except OSError as e:
-            #print "Exeception while running Arduino:", e
-            #sys.exit(1)
-
-finally:
-    # restore the original file
-    print "Restoring original sketch file"
-    with open(ino, "w") as fid:
-        fid.write("".join(original))
+            fid.write("".join(original))
 
 
+for subdir, dirs, files in os.walk("examples"):
+    for f in files:
+        RunAllConfigs( os.path.join(subdir, f) )
 
-
-# Original bash script:
-#for ino in `find examples/ | grep ino`; do
-#    echo "Verifying $ino"
-#    $ARDUINO --verify --board arduino:avr:uno $ino
-#done
 
