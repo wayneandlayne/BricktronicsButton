8dacd02f45f7bc11fddb103720581f79e2554c19
Matthew Beckler <matthew.beckler@gmail.com>
2016-08-16 15:56:08 -0500

    Removed external dependencies

--- examples/SimpleButton/SimpleButtonBricktronicsShield/SimpleButtonBricktronicsShield.ino
+++ examples/SimpleButton/SimpleButtonBricktronicsShield/SimpleButtonBricktronicsShield.ino
@@ -1,19 +1,19 @@
-// Bricktronics Example: SimpleButtonBricktronicsBreakoutBoard
+// Bricktronics Example: SimpleButtonBricktronicsShield
 // http://www.wayneandlayne.com/bricktronics
 //
 // This example queries the NXT pushbutton sensor and transmits "pressed" and
 // "released" over the serial port.
 //
 // Hardware used:
-// * Wayne and Layne Bricktronics Breakout Board
-//   https://store.wayneandlayne.com/products/bricktronics-breakout-board.html
+// * Wayne and Layne Bricktronics Shield
+//   https://store.wayneandlayne.com/products/bricktronics-shield-kit.html
 // * LEGO NXT Pushbutton sensor
 //
 // Software libraries used:
 // * Wayne and Layne BricktronicsButton library
 //   https://github.com/wayneandlayne/BricktronicsButton
-// * Adafruit MCP23017 library
-//   https://github.com/adafruit/Adafruit-MCP23017-Arduino-Library
+// * Wayne and Layne BricktronicsShield library
+//   https://github.com/wayneandlayne/BricktronicsShield
 //
 // Written in 2016 by Matthew Beckler and Adam Wolf for Wayne and Layne, LLC
 // To the extent possible under law, the author(s) have dedicated all
@@ -24,27 +24,22 @@
 
 
 // Include the Bricktronics libraries
+#include <BricktronicsShield.h>
 #include <BricktronicsButton.h>
 
 
-// Connect these pins on the Bricktronics Breakout board:
-//  Pin 1 - Connect to Arduino input pin (any digital input pin)
-//  Pin 2 - Connect to Ground
-//  Pin 3 - Connect to Ground
-//  Pin 4 - Connect to 5V
-//  Pin 5 - No connection
-//  Pin 6 - No connection
-//
-// The BricktronicsButton() argument is simply the Arduino pin where pin 1
-// on the breakout board is connected.
-//
-BricktronicsButton b(7);
+// Select the sensor port (SENSOR_1 through SENSOR_4) in the constructor below.
+// If your chosen sensor port has jumpers (ports 3 and 4), connect pins 2-3 and 4-5.
+BricktronicsButton b(BricktronicsShield::SENSOR_1);
 
 
 void setup()
 {
   // Be sure to set your serial console to 115200 baud
   Serial.begin(115200);
+
+  // Initialize the Bricktronics Shield
+  BricktronicsShield::begin();
 
   // Initialize the button connection
   b.begin();
